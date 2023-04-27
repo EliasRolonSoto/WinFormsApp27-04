b93484b309fd2f1f5d588eaebd1598f84dfefaa7
Elias <eliasrolonsoto@gmail.com>
2023-04-27 19:01:47 -0300

    Actualizado y "funcionando"

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,10 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             listBox1 = new ListBox();
             btnBuscar = new Button();
             txtSearch = new TextBox();
             listView1 = new ListView();
+            Nombre = new ColumnHeader();
+            asunto = new ColumnHeader();
+            lblSeleccion = new Label();
+            txtSeleccion = new TextBox();
+            btnGuardar = new Button();
+            checkedListBox1 = new CheckedListBox();
+            dataGridView1 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            emailRepositoryBindingSource = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)emailRepositoryBindingSource).BeginInit();
             SuspendLayout();
             // 
             // listBox1
@@ -64,17 +77,82 @@
             // 
             // listView1
             // 
+            listView1.Columns.AddRange(new ColumnHeader[] { Nombre, asunto });
             listView1.Location = new Point(295, 140);
             listView1.Name = "listView1";
             listView1.Size = new Size(149, 259);
             listView1.TabIndex = 3;
             listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // lblSeleccion
+            // 
+            lblSeleccion.AutoSize = true;
+            lblSeleccion.Location = new Point(84, 457);
+            lblSeleccion.Name = "lblSeleccion";
+            lblSeleccion.Size = new Size(57, 15);
+            lblSeleccion.TabIndex = 4;
+            lblSeleccion.Text = "Seleccion";
+            // 
+            // txtSeleccion
+            // 
+            txtSeleccion.Location = new Point(84, 475);
+            txtSeleccion.Name = "txtSeleccion";
+            txtSeleccion.Size = new Size(239, 23);
+            txtSeleccion.TabIndex = 5;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(86, 514);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(93, 30);
+            btnGuardar.TabIndex = 6;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(468, 140);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(145, 256);
+            checkedListBox1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, Column2 });
+            dataGridView1.Location = new Point(645, 140);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(317, 259);
+            dataGridView1.TabIndex = 8;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "nombredg";
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "asunto dg";
+            Column2.Name = "Column2";
+            // 
+            // emailRepositoryBindingSource
+            // 
+            emailRepositoryBindingSource.DataSource = typeof(App.Core.Data.EmailRepository);
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(584, 511);
+            ClientSize = new Size(1016, 586);
+            Controls.Add(dataGridView1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(btnGuardar);
+            Controls.Add(txtSeleccion);
+            Controls.Add(lblSeleccion);
             Controls.Add(listView1);
             Controls.Add(txtSearch);
             Controls.Add(btnBuscar);
@@ -82,6 +160,8 @@
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)emailRepositoryBindingSource).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -92,5 +172,15 @@
         private Button btnBuscar;
         private TextBox txtSearch;
         private ListView listView1;
+        private Label lblSeleccion;
+        private TextBox txtSeleccion;
+        private Button btnGuardar;
+        private CheckedListBox checkedListBox1;
+        private DataGridView dataGridView1;
+        private ColumnHeader Nombre;
+        private ColumnHeader asunto;
+        private BindingSource emailRepositoryBindingSource;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn Column2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -4,7 +4,9 @@ using App.Core.Entities;
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
+
     {
+        private Email _emailSeleccionado;
         public Form1()
         {
             InitializeComponent();
@@ -16,27 +18,78 @@ namespace WinFormsApp1
         }
         private void CargarDatos()
         {
+
+            txtSeleccion.Text = string.Empty;
             var emails = (new EmailRepository()).Buscar(txtSearch.Text);
 
 
+            //Listbox
             listBox1.DataSource = emails;
-            listBox1.ValueMember = "Asunto";
+            listBox1.DisplayMember = "Asunto";
+            //listBox1.ValueMember = "Id";
 
 
+            //ListView
+            listView1.View = View.Details;
+            listView1.Items.Clear();
+            foreach (var email in emails)
+            {
+                ListViewItem item = new ListViewItem(email.Asunto);
+                item.ForeColor = Color.Red;
+                item.Checked = true;
+
+                item.SubItems.Add(email.Id.ToString());
+                listView1.Items.Add(item);
+            }
+
+
+            //checkedListBox
+            checkedListBox1.Items.Clear();
+            foreach (var email in emails)
+            {
+                checkedListBox1.Items.Add(email.Asunto);
+            }
+
+            //DataGrid
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = emails;
 
 
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
-        }
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
             CargarDatos();
         }
 
-        
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            _emailSeleccionado.Asunto = txtSeleccion.Text;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+
+                var elementoSeleccionado = listView1.SelectedItems[0]; //.ToString();
+
+                txtSeleccion.Text = elementoSeleccionado.Text;
+
+                var aux = elementoSeleccionado.SubItems[0].Text;
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _emailSeleccionado = ((Email)listBox1.SelectedItem);
+
+            txtSeleccion.Text = _emailSeleccionado.Asunto;
+
+
+        }
     }
 }
